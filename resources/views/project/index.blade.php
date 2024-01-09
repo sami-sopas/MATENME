c55797fc0df6b7e1ba8c4902e9a79d6b0e0330a3
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-01-09 10:41:23 -0600

    Grid spacing & cards design

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -1,21 +1,22 @@
 <x-app-layout>
     <header class="flex items-center mb-3 py-4">
 
-        <div class="flex justify-between">
+        <div class="flex justify-between w-full items-center">
             <h3 class="mr-auto text-gray-600 text-xl">My Projects</h3>
-            <a href="/projects/create" class="text-gray-500">New Project</a>
+            <a href="/projects/create" class="button">New Project</a>
         </div>
-
 
     </header>
 
-    <main class="flex flex-wrap -mx-3">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-        <div class="w-1/3 px-3 pb-6">
-            <div class="bg-white p-3 rounded-md shadow-md" style="height: 200px">
-                <h3 class="text-2xl mb-6 py-4">{{ $project->title }}</h3>
+        <div class="lg:w-1/3 px-3 pb-6">
+            <div class="bg-white p-3 rounded-lg shadow-md" style="height: 200px">
+                <h3 class="text-2xl mb-6 py-4 -ml-3 border-l-4 border-blue-300 pl-4">
+                    <a href="{{$project->path()}}">{{ $project->title }}</a>
+                </h3>
 
-                <div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>
+                <div class="text-gray-600 pl-3">{{ Str::limit($project->description, 100) }}</div>
             </div>
         </div>
         @empty
